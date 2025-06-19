3fbece35cad218c307fd6655c9b047d286fcdebf
GINO JAMES <ginojames27@gmail.com>
2025-06-19 12:28:25 +0530

    few changes after tut 3

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -1,12 +1,29 @@
 import Header from "./Header"
+import React, { useState } from "react";
+import "./App.css"
 function Apple()
+{
+  const [counter,setcounter]=useState(0);
+  return(
+    <>
+      <Header />// component imported from Header.jsx from differnt file
+      <h1>counter:{counter}</h1>
+      <button onClick={()=>setcounter(counter+1)}>Increase</button>
+      <button onClick={()=>setcounter(counter-1)}>decrease</button>
+      <Food/>
+
+      
+
+    </>
+  )
+}
+function Food()//component within the same file
 {
   return(
     <div>
-      <Header />
-      <h1>Hellooo Gino</h1>
-      <h2>hii</h2>
+      <h1>Food</h1>
+      <p>Food is essential for life.</p>
     </div>
   )
 }
-export default Apple
+export default Apple;
--- react-blog/src/Header.jsx
+++ react-blog/src/Header.jsx
@@ -3,7 +3,7 @@ function Header()
         return(
             <div>
             <h1>hiii</h1>
-            <h2>hii</h2>
+            <h2>gino</h2>
             </div>
         )
     }
